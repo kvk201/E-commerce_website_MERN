e1290987b2c803481a2f85a08b77b5a1c6b38cb7
messi618 <54788160+messi618@users.noreply.github.com>
2022-02-02 21:12:33 -0800

    changes

--- assignment1/server/server.js
+++ assignment1/server/server.js
@@ -1,7 +1,8 @@
     const express = require("express");
 
     const app = express();
-    app.use(express.static('public'));
+    app = express.static('public');
+    app.use('/', app);
 
     app.listen(3000, function() {
         console.log('app is running on port 3000!')
--- assignment1/src/App.jsx
+++ assignment1/src/App.jsx
@@ -1,5 +1,4 @@
-    //
-    const nameComponent = (
+     const nameComponent = (
     <div id ="name_out">
             <h2 id = "name_component">Vivek Kumar</h2>
     </div>
@@ -16,8 +15,7 @@
             <p id = "intro_component">
             On insensible possession oh particular attachment at excellence in. The books arose but miles happy she. It building contempt or interest children mistress of unlocked no. Offending she contained mrs led listening resembled. Delicate marianne absolute men dashwood landlord and offended. Suppose cottage between and way. Minuter him own clothes but observe country. Agreement far boy otherwise rapturous incommode favourite.
             </p>
-        </div>
-                
+        </div>              
     );
 
     const buttonComponent = (

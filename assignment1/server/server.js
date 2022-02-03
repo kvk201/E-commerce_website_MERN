9517d788c94678a26840645273bab30a67323755
messi618 <54788160+messi618@users.noreply.github.com>
2022-02-02 21:13:16 -0800

    update

--- assignment1/server/server.js
+++ assignment1/server/server.js
@@ -1,8 +1,7 @@
     const express = require("express");
 
     const app = express();
-    app = express.static('public');
-    app.use('/', app);
+    app.use(express.static('public'));
 
     app.listen(3000, function() {
         console.log('app is running on port 3000!')
